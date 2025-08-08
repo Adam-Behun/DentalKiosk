89cf8363e6775f69df4437e26829b5b1944d574c
Adam Behun <adam.behun@slu.edu>
2025-08-08 13:08:20 -0400

    updated .gitignore

--- backend/routes/mfa.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { sendMfaCode, verifyMfaCode } = require('../controllers/mfaController');
-const router = express.Router();
-
-router.post('/request', sendMfaCode); // New route for requesting the MFA code
-router.post('/verify', verifyMfaCode);
-
-module.exports = router;
